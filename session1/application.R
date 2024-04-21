bf370a70b1de475dacd4ae97fe0704865b1ea799
jhonrolyol <rolyordonezleon@gmail.com>
2024-04-20 22:55:42 -0500

    Modified on 21/04/2024

---
+++ session1/application.R
@@ -0,0 +1,48 @@
+#------------------------------------------------------------------------------#
+#                        ECONOMIC ANALYSIS WITH R
+#
+#                     Introduction to programming in R
+#
+#                           Jhon R. Ordoñez 
+#                           April 21, 2024
+#
+#------------------------------------------------------------------------------#
+
+# We clean the work environment -------------------------------------------
+  rm(list = ls())
+
+# Shortcuts ---------------------------------------------------------------
+  # ctrl + s: save
+  # ctrl + shift + r: new section
+  # ctrl + shift + p: show command palette
+  # Alt + shift + k: keyboard Shortcuts Help
+  # ...
+
+# Install packages --------------------------------------------------------
+  #install.packages("readxl")
+  #install.packages("ggplot2")
+  #install.packages("deSolve")
+  #install.packages("nortest")
+  #install.packages("tseries")
+  #install.packages("FinTS")
+  #install.packages("RcmdrMisc")
+  #install.packages("pastecs")
+  #install.packages("xts")
+
+# load libraries ----------------------------------------------------------
+  library(readxl)
+  library(ggplot2)
+  library(deSolve)
+  library(nortest)
+  library(tseries)
+  library(FinTS)
+  library(RcmdrMisc)
+  library(pastecs)
+  library(xts)
+
+# Create folders from Rstudio ---------------------------------------------
+  dir.create("data")
+  dir.create("figures")
+  dir.create("tables")
+ 
+  
